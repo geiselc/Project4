ac985784b49156bf70995003bf07751edd5e3f1a
geiselc <geiselc@mail.gvsu.edu>
2014-10-17 21:07:59 -0400

    Header Fields update

--- src/OverlayClient.java
+++ src/OverlayClient.java
@@ -48,77 +48,77 @@ public class OverlayClient {
 			/** Build the UDP Header */
 			udpHeader.setSrcPort("0010011010010100");	// 9876
 			udpHeader.setDstPort("0010011010010100");	// 9876
-			
-			/* Need to get proper padded length of data */
-			String zero8 = "00000000";
-			String padString = "";
-			int messageLength = message.getBytes().length * 8;
-			int pad = messageLength % 2;
-			
-			if (pad == 1) {
-				padString = zero8;
-			}
-			
-			
-			int udpLength = 12 + 8 + messageLength + pad;
+				/* Need to get proper padded length of data */
+				String zero8 = "00000000";
+				String padString = "";
+				int messageLength = message.getBytes().length * 8;
+				int pad = messageLength % 2;
+				
+				if (pad == 1) {
+					padString = zero8;
+				}
+				
+				
+				int udpLength = 12 + 8 + messageLength + pad;
 			udpHeader.setLength(udpLength);
+			// checksum
 			
-/** TODO The old code is listed below: **/
-			String zero8 = "00000000";
-			String protocol = "00010001";
-			String src = InetAddress.getLocalHost().toString();
-			String dst = dstIP;
-			String padString = "";
-			
-			/* src & dst ports */
-			String port = "0010011010010100";
-			int messageLength = message.getBytes().length * 8;
-			int pad = messageLength % 2;
-			
-			if (pad == 1) {
-				padString = zero8;
-			}
-			
-			
-			int udpLength = 12 + 8 + messageLength + pad;
-			String udpCheckSum = "";
-			String data = message;
-			
-			byte[] by = src.getBytes();
-			
-			StringBuilder binary = new StringBuilder();
-			  for (byte b : by)
-			  {
-			     int val = b;
-			     for (int i = 0; i < 8; i++)
-			     {
-			        binary.append((val & 128) == 0 ? 0 : 1);
-			        val <<= 1;
-			     }
-			  }
-			String srcBits = binary.toString();
-			
-			by = dst.getBytes();
-			
-			binary = new StringBuilder();
-			  for (byte b : by)
-			  {
-			     int val = b;
-			     for (int i = 0; i < 8; i++)
-			     {
-			        binary.append((val & 128) == 0 ? 0 : 1);
-			        val <<= 1;
-			     }
-			  }
-			String destBits = binary.toString();
-			
-			while(srcBits.length() < 32) {
-				srcBits = "0" + srcBits;
-			}
-			
-			while(destBits.length() < 32) {
-				destBits = "0" + destBits;
-			}
+/** The old code is listed below: **/
+//			String zero8 = "00000000";
+//			String protocol = "00010001";
+//			String src = InetAddress.getLocalHost().toString();
+//			String dst = dstIP;
+//			String padString = "";
+//			
+//			/* src & dst ports */
+//			String port = "0010011010010100";
+//			int messageLength = message.getBytes().length * 8;
+//			int pad = messageLength % 2;
+//			
+//			if (pad == 1) {
+//				padString = zero8;
+//			}
+//			
+//			
+//			int udpLength = 12 + 8 + messageLength + pad;
+//			String udpCheckSum = "";
+//			String data = message;
+//			
+//			byte[] by = src.getBytes();
+//			
+//			StringBuilder binary = new StringBuilder();
+//			  for (byte b : by)
+//			  {
+//			     int val = b;
+//			     for (int i = 0; i < 8; i++)
+//			     {
+//			        binary.append((val & 128) == 0 ? 0 : 1);
+//			        val <<= 1;
+//			     }
+//			  }
+//			String srcBits = binary.toString();
+//			
+//			by = dst.getBytes();
+//			
+//			binary = new StringBuilder();
+//			  for (byte b : by)
+//			  {
+//			     int val = b;
+//			     for (int i = 0; i < 8; i++)
+//			     {
+//			        binary.append((val & 128) == 0 ? 0 : 1);
+//			        val <<= 1;
+//			     }
+//			  }
+//			String destBits = binary.toString();
+//			
+//			while(srcBits.length() < 32) {
+//				srcBits = "0" + srcBits;
+//			}
+//			
+//			while(destBits.length() < 32) {
+//				destBits = "0" + destBits;
+//			}
 			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
